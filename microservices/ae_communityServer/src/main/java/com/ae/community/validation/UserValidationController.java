0d7ceddf4f56c2ca07f4563063dd1524e39b8e68
Vector1331 <tpgus4796@gmail.com>
2022-11-14 23:15:53 +0900

    [Fix] validation 코드 삭제

--- microservices/ae_communityServer/src/main/java/com/ae/community/validation/UserValidationController.java
+++ microservices/ae_communityServer/src/main/java/com/ae/community/validation/UserValidationController.java
@@ -11,17 +11,6 @@ import static com.ae.community.exception.CodeAndMessage.*;
 @Controller
 @RequiredArgsConstructor
 public class UserValidationController {
-
-
-    public void validateUserByJwt(String jwtUserId) {
-        if(jwtUserId.equals("INVALID JWT")) throw new chaebbiException(INVALID_JWT);
-        if(jwtUserId.equals("anonymousUser")) throw new chaebbiException(EMPTY_JWT);
-    }
-
-    public void compareUserIdAndJwt(Long userIdx, String jwtUserId) {
-        if(!jwtUserId.equals(userIdx.toString())) throw new chaebbiException(NOT_CORRECT_JWT_AND_PATH_VARIABLE);
-    }
-
     public void validateUserByUserIdxAndJwt(Long userIdx, HashMap<String, String> user) {
         if(user.get("userIdx").equals("INVALID JWT-USERIDX")) throw new chaebbiException(INVALID_JWT_USERIDX);
         if(user.equals("anonymousUser")) throw new chaebbiException(EMPTY_JWT);
