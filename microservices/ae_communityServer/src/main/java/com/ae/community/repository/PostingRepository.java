cb3d6c62bb7af2960b5132a9d7ec1b22503d548e
rosesua318 <rosesua@duksung.ac.kr>
2022-11-14 21:01:08 +0900

    [Fix] 내가 스크랩한 게시글 쿼리문에 CommunityUser 삭제 (김민서)

--- microservices/ae_communityServer/src/main/java/com/ae/community/repository/PostingRepository.java
+++ microservices/ae_communityServer/src/main/java/com/ae/community/repository/PostingRepository.java
@@ -23,10 +23,8 @@ public interface PostingRepository extends JpaRepository<Posting, Long>, CrudRep
 
     Page<Posting> findAllByUserIdx(Long userIdx, Pageable pageable);
 
-    @Query(value = "select p from Posting p where p.idx IN (select sc.postIdx from Scrap sc " +
-            "join CommunityUser u on sc.userIdx = u.userIdx where u.userIdx = :userIdx)",
-    countQuery = "select COUNT(p) from Posting p where p.idx IN (select sc.postIdx from Scrap sc " +
-            "join CommunityUser u on sc.userIdx = u.userIdx where u.userIdx = :userIdx)")
+    @Query(value = "select p from Posting p join Scrap sc on sc.postIdx = p.idx where sc.userIdx = :userIdx",
+    countQuery = "select COUNT(p) from Posting p join Scrap sc on sc.postIdx = p.idx where sc.userIdx = :userIdx")
     Page<Posting> findAllWithScrap(@Param("userIdx") Long userIdx, Pageable pageable);
 
     @Query(value = "select p from Posting p ORDER BY p.idx")
