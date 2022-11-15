7079d969d5fcbecbad2e676bf6fd8cb53542c563
rosesua318 <rosesua@duksung.ac.kr>
2022-11-15 22:10:04 +0900

    [Feat] eureka client instance1 추가

--- microservices/ae_SpringServer/src/main/java/com/ae/ae_SpringServer/AeSpringServerApplication.java
+++ microservices/ae_SpringServer/src/main/java/com/ae/ae_SpringServer/AeSpringServerApplication.java
@@ -2,10 +2,12 @@ package com.ae.ae_SpringServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AeSpringServerApplication {
 
