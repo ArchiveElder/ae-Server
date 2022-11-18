5fa3f287fecf320ba095721b2656be4e2a7badae
Vector1331 <tpgus4796@gmail.com>
2022-11-18 19:27:02 +0900

    [Refacotr] v1,v2 사용자 코드 삭제

--- microservices/ae_SpringServer/src/main/java/com/ae/ae_SpringServer/config/s3/S3Uploader.java
+++ microservices/ae_SpringServer/src/main/java/com/ae/ae_SpringServer/config/s3/S3Uploader.java
@@ -1,4 +1,4 @@
-package com.ae.ae_SpringServer.aws;
+package com.ae.ae_SpringServer.config.s3;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
